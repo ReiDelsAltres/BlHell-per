0c662a3e2262dadca683801de92ab9b2ac50d0c9
ReiDelsAltres <velibecovv@gmail.com>
2025-03-14 02:57:29 +0400

    - Constructor page updated to BETA with basic functional

--- Program.cs
+++ Program.cs
@@ -1,3 +1,4 @@
+using BlazorDownloadFile;
 using BlHell_per;
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
@@ -8,6 +9,7 @@ builder.RootComponents.Add<HeadOutlet>("head::after");
 
 Static.HttpClient = new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) };
 builder.Services.AddScoped(sp => Static.HttpClient);
+builder.Services.AddBlazorDownloadFile(ServiceLifetime.Scoped);
 
 await builder.Build().RunAsync();
 
