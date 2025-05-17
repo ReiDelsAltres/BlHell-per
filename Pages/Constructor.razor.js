776797afd90a7c7806c6b269c1c48e10b57ad517
ReiDelsAltres <ReiDelsAltres@users.noreply.github.com>
2025-05-17 07:40:59 +0000

    deploy: da290917be6c6c8474bb0dc8d3ab99bcfb5d4619

---
+++ Pages/Constructor.razor.js
@@ -0,0 +1,19 @@
+﻿function downloadFileFromStream(filename, contentStreamReference) {
+    const arrayBuffer = await contentStreamReference.arrayBuffer();
+    const blob = new Blob([arrayBuffer]);
+    const url = URL.createObjectURL(blob);
+    const anchorElement = document.createElement('a');
+    anchorElement.href = url;
+    anchorElement.download = fileName ?? '';
+    anchorElement.click();
+    anchorElement.remove();
+    URL.revokeObjectURL(url);
+}
+function saveFile(file, Content) {
+    var link = document.createElement('a');
+    link.download = name;
+    link.href = "data:text/plain;charset=utf-8," + encodeURIComponent(Content)
+    document.body.appendChild(link);
+    link.click();
+    document.body.removeChild(link);
+}
