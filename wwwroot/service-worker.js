42913454145f13eb7be03a58aaa59322f69bcfeb
ReiDelsAltres <velibecovv@gmail.com>
2025-06-13 01:23:10 +0400

    i

--- wwwroot/service-worker.js
+++ wwwroot/service-worker.js
@@ -3,7 +3,3 @@
 // be reflected on the first load after each change).
 
 //////////////////////////
-
-// Caution! Be sure you understand the caveats before publishing an application with
-// offline support. See https://aka.ms/blazor-offline-considerations
-
